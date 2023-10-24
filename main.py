98c8ef45c2e9d372ff422ff4caf0bfb8096bf355
luca <luca.barsottini@outlook.it>
2023-10-24 23:33:41 +0200

    wip

--- main.py
+++ main.py
@@ -2,31 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 
-def clean_dataset(dataset):
-    print('start cleaning')
-
-    for i in dataset:
-        dataset[i] = dataset[i][ dataset[i]['electrosmog'].astype(str).__contains__('-') == False ]
-        i.to_csv('./semi_clean_file/'+str(i))
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    path = './resources/'
+    path = './resources/concat_files/all_pm_meteo_153.csv'
 
-    dir_list = os.listdir(path)
-    pool = {}
-    for file in dir_list:
-        if file.endswith('.csv'):
-            print(file.split('.')[0].split('_')[1].replace('0', '') + '_' + file.split('.')[0].split('_')[0])
-            pool[file.split('.')[0].split('_')[1].replace('0', '') + '_' + file.split('.')[0].split('_')[
-                0]] = pd.read_csv(path + file, sep=';', engine='python')
-# CAP133_1
-# CAP153_1
-# CAP133_2
-# CAP153_2
-# CAP133_3
-# CAP153_3
-    clean_dataset(pool)
+    all_meteo = pd.read_csv(path)
+
+    print('end')
+
 
 
 
